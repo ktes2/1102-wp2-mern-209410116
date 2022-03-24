84ff658088840a192cc9039cb827d0f0e2dee933
ktes2 <a0035044@gmail.com>
2022-03-24 21:13:51 +0800

    first commit

---
+++ classdemo/md1231/w5/step1_Initial_Setup/src/App_16.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>MERN DEMO --陳胤 209410116</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client_16/src/App_16.js
@@ -0,0 +1,15 @@
+import Landing_16 from "./pages/Landing_16";
+import styled from "styled-components";
+
+
+
+
+function App_16() {
+  return (
+    <div className="App">
+        <Landing_16></Landing_16>
+    </div>
+  );
+}
+
+export default App_16;

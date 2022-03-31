9fdf995aa15273ae42f05a9e432051af3c9bb316
ktes2 <a0035044@gmail.com>
2022-03-31 21:14:17 +0800

    w6 md update

---
+++ classdemo/w6/Step2_404page_Register_password/src/components/index.js
@@ -0,0 +1,4 @@
+import Logo_16 from "./Logo_16";
+import FormRow_16 from "./FormRow_16";
+
+export {Logo_16,FormRow_16};
---
+++ classdemo/w6/Step2_404page_Register_password/src/pages/index.js
@@ -0,0 +1,10 @@
+import Landing_16 from "./Landing_16";
+import Dashboard_16 from "./Dashboard_16";
+import Register_16 from "./Register_16";
+import Error_16 from "./Error_16";
+
+
+export {Landing_16};    
+export {Dashboard_16};
+export {Register_16};
+export {Error_16};
